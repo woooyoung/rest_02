35af2e36f6095bcacb75d791d49d206430e31fd5
woooyoung <axdsw121@gmail.com>
2025-02-11 16:23:54 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/rest_02/Rest02Application.java
@@ -0,0 +1,13 @@
+package com.koreait.rest_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest02Application.class, args);
+    }
+
+}
